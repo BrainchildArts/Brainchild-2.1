f4b9bc5c3e65fd3296719c2ef2ce1ef96864fd32
Jerome Toole <jerometoole@gmail.com>
2016-03-26 15:28:22 +0000

    simplify single templates

--- single.php
+++ single.php
@@ -1,8 +1,1 @@
-<?php
-if(has_term('blog', 'category', $post)) {
-   get_template_part('single-blog', 'blog');
-} else {
-   // use default template file single-template.php
-   get_template_part('templates/content-single', get_post_type());
-}
-?>
+<?php get_template_part('templates/content-single', get_post_type()); ?>
