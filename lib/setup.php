42b97eb32379149feef8a1b522d6fcbc820c6c62
Jerome Toole <jerometoole@gmail.com>
2016-04-02 09:04:14 +0100

    Grid working

--- lib/setup.php
+++ lib/setup.php
@@ -110,6 +110,8 @@ function assets() {
 
   wp_enqueue_script('vendor/js', Assets\asset_path('scripts/vendor.js'), ['jquery'], null, true);
 
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['jquery'], null, true);
+  wp_enqueue_script('grid', Assets\asset_path('scripts/grid.js'), ['vendor/js'], null, true);
+
+  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), ['grid'], null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
--- templates/content-lineup.php
+++ templates/content-lineup.php
@@ -2,23 +2,6 @@
   <a href="<?php the_permalink(); ?>" data-largesrc="images/2.jpg" data-title="Veggies sunt bona vobis" data-description="Komatsuna prairie turnip wattle seed artichoke mustard horseradish taro rutabaga ricebean carrot black-eyed pea turnip greens beetroot yarrow watercress kombu.">
     <div class="entry-image">
       <?php if ( has_post_thumbnail() ) { the_post_thumbnail('grid-square');} ?>
-      <h2 class="entry-title"><?php the_title(); ?></h2>
     </div>
   </a>
-
-  <div class="artist__expander" style="display: none">
-    <div class="artist__expander-inner">
-      <span class="artist__close"></span>
-      <div class="artist__fullimg">
-        <div class="artist__loading"></div>
-        <?php if ( has_post_thumbnail() ) { the_post_thumbnail('grid-square');} ?>
-      </div>
-      <div class="entry-text artist__details">
-        <div class="entry-content">
-          <?php the_content(); ?>
-        </div>
-        <a href="http://cargocollective.com/jaimemartinez/">Visit website</a>
-      </div>
-    </div>
-  </div>
 </article>
